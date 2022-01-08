14e665f222d19e8e368895fc829ba85b47ea3b72
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-01-08 20:01:50 +0530

    Solution for button and search filter

--- filter-project-main/js/app.js
+++ filter-project-main/js/app.js
@@ -1,47 +1,91 @@
+//First Solution
+//const buttons = document.querySelectorAll("a.btn");
+//const storeItem = document.querySelectorAll(".store-item");
+
+// buttons.forEach(function (button) {
+//   button.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     if (button.getAttribute("data-filter") === "all") {
+//       storeItem.forEach(function (item) {
+//         item.style.display = "block";
+//       });
+//     } else if (button.getAttribute("data-filter") === "cakes") {
+//       storeItem.forEach(function (item) {
+//         if (item.classList.contains("cakes")) {
+//           item.style.display = "block";
+//         } else {
+//           item.style.display = "none";
+//         }
+//       });
+//     } else if (button.getAttribute("data-filter") === "cupcakes") {
+//       storeItem.forEach(function (item) {
+//         if (item.classList.contains("cupcakes")) {
+//           item.style.display = "block";
+//         } else {
+//           item.style.display = "none";
+//         }
+//       });
+//     } else if (button.getAttribute("data-filter") === "sweets") {
+//       storeItem.forEach(function (item) {
+//         if (item.classList.contains("sweets")) {
+//           item.style.display = "block";
+//         } else {
+//           item.style.display = "none";
+//         }
+//       });
+//     } else {
+//       storeItem.forEach(function (item) {
+//         if (item.classList.contains("doughnuts")) {
+//           item.style.display = "block";
+//         } else {
+//           item.style.display = "none";
+//         }
+//       });
+//     }
+//   });
+// });
+
+//2nd solution
+
 const buttons = document.querySelectorAll("a.btn");
-//console.log(buttons);
-
-const url = "./img/";
-
-let imgArray = [
-  "cake-1",
-  "cake-2",
-  "cake-3",
-  "cupcake-1",
-  "cupcake-2",
-  "cupcake-3",
-  "doughnut-1",
-  "doughnut-2",
-  "doughnut-3",
-  "headerBcg",
-  "sweets-1",
-  "sweets-2",
-  "sweets-3",
-  "z-cake-2",
-  "z-sweets-3",
-];
-
-function myfunction(justVar) {}
+const storeItem = document.querySelectorAll(".store-item");
 
 buttons.forEach(function (button) {
   button.addEventListener("click", function (e) {
     e.preventDefault();
-    console.log(button);
-    if (button.getAttribute("data-filter") === "all") {
-      console.log("all");
-    } else if (button.getAttribute("data-filter") === "cakes") {
-      console.log("cakes");
-      let cakeArray = [];
-      let res = imgArray.filter(() =>
-        imgArray.includes(imgArray.values === "cake")
-      );
-      console.log(res);
-    } else if (button.getAttribute("data-filter") === "cupcakes") {
-      console.log("cupcakes");
-    } else if (button.getAttribute("data-filter") === "sweets") {
-      console.log("sweets");
+    let filter = e.target.dataset.filter;
+    console.log(filter);
+    storeItem.forEach(function (item) {
+      if (filter === "all") {
+        //console.log(item);
+        item.style.display = "block";
+      } else {
+        //console.log(filter, item.dataset.item);
+        if (item.classList.contains(filter)) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      }
+    });
+  });
+});
+
+//search type box logic
+
+const searchBox = document.querySelector("#search-item");
+console.log(searchBox);
+
+searchBox.addEventListener("keyup", (e) => {
+  console.log(e);
+  //e.preventDefault();
+  const searchFilter = e.target.value.toLowerCase().trim();
+  console.log(searchFilter);
+  storeItem.forEach(function (item) {
+    if (item.textContent.includes(searchFilter)) {
+      item.style.display = "block";
     } else {
-      console.log("Doughnots");
+      item.style.display = "none";
     }
   });
 });
