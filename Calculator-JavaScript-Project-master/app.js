9cb72d8b8a5bf8e13566d349ee2f49b14a9031e4
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-01-14 01:06:27 +0530

    added logic for calculator file

--- Calculator-JavaScript-Project-master/app.js
+++ Calculator-JavaScript-Project-master/app.js
@@ -1,6 +1,8 @@
 const buttons = document.querySelectorAll(".btn");
 const input = document.querySelector(".screen");
-console.log(input);
+const equal = document.querySelector(".btn-equal");
+const clear = document.querySelector(".btn-clear");
+console.log(clear);
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -8,4 +10,17 @@ buttons.forEach((button) => {
     input.value += value;
     //input.value = e.target.dataset.num;
   });
+});
+
+equal.addEventListener("click", (e) => {
+  if (input.value === " ") {
+    input.value = "please enter some value";
+  } else {
+    let answer = eval(input.value);
+    input.value = answer;
+  }
+});
+
+clear.addEventListener("click", (e) => {
+  input.value = "";
 });
