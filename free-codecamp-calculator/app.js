c9f68b26e074962c76cc8aec6d011cdad3f5f6b2
saurin1301 <saurin1301@gmail.com>
2022-01-14 18:38:06 +0530

    added js file with clear,number functionality

--- free-codecamp-calculator/app.js
+++ free-codecamp-calculator/app.js
@@ -1,7 +1,43 @@
 const calculator = document.querySelector(".calculator");
 const keys = document.querySelector(".calculator__keys");
-console.log(calculator, keys);
+const display = document.querySelector(".calculator__display");
+//console.log(calculator, keys);
+console.log(display);
 
 keys.addEventListener("click", (e) => {
-  console.log(e.target.matches("button"));
+  if (e.target.matches("button")) {
+    //console.log("button");
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayNum = display.textContent;
+    //console.log(keyContent, displayNum);
+    if (!action) {
+      //console.log("Number key");
+      if (displayNum === "0") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent += keyContent;
+      }
+    }
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      console.log("operator key");
+    }
+    if (action === "decimal") {
+      //console.log("decimal key");
+      display.textContent = displayNum + ".";
+    }
+    if (action === "clear") {
+      display.textContent = "0";
+      //console.log("clear key");
+    }
+    if (action == "calculate") {
+      console.log("equal key");
+    }
+  }
 });
